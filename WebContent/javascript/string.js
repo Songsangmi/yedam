44192e71ec3c65e23c5e2d2e6495ae7b2352be88
viole <viole@SONG>
2021-10-14 14:58:49 +0900

    commit

--- WebContent/javascript/string.js
+++ WebContent/javascript/string.js
@@ -2,24 +2,9 @@
 'hello'.substring(0, 3);
 'hello'.substr(1, 2);
 
-// 970101-2345678
-// 990301-1234567
-// 000503-3456789
-// 010914567890
-
 function checkGender(birthInfo) {
     //2000년 01.01 이전 출생자 주민번호 뒷자리(1:남자, 2:여자)
     //2000년 01.01 이후 출생자 주민번호 뒷자리(3:남자, 4:여자)
-
-    //     let gen = birthInfo.substr(7, 1);
-    //     if (gen==1 || gen==3) {
-    //         console.log("남자");
-    //     } else if (gen==2 || gen==4) {
-    //         console.log("여자");
-    //     }else
-    //         console.log("오류");
-    //     return '남자' | '여자';
-    // }
 
     let today = new Date();
     console.log(today.getFullYear());
